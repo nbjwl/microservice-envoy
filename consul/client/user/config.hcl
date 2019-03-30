128a19fa55decbd75ec7739ce0baa0b002e62077
taki <nbjwl@live.com>
2019-03-30 21:07:08 +0800

    envoy demo

--- consul/client/user/config.hcl
+++ consul/client/user/config.hcl
@@ -3,10 +3,14 @@ client_addr = "0.0.0.0"
 ports {
   grpc = 8502
 }
+connect {
+  enabled = true
+}
 services {
   name = "user"
   port = 8080
   connect {
-    sidecar_service {}
+    sidecar_service {
+    }
   }
 }
--- java/demo/src/main/java/com/nbjwl/microservice/envoy/demo/ServiceConfiguation.java
+++ java/demo/src/main/java/com/nbjwl/microservice/envoy/demo/ServiceConfiguation.java
@@ -3,6 +3,7 @@ package com.nbjwl.microservice.envoy.demo;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestTemplate;
 
 
@@ -12,6 +13,13 @@ public class ServiceConfiguation
     @Bean
     public RestTemplate demoRestTemplate()
     {
-        return new RestTemplateBuilder().rootUri("http://localhost:10000").build();
+        return new RestTemplateBuilder().rootUri("http://localhost:10000")
+                .additionalInterceptors((request, body, execution) ->
+                {
+
+                    HttpHeaders headers = request.getHeaders();
+                    headers.set("SERVICE", "service:user");
+                    return execution.execute(request, body);
+                }).build();
     }
 }
