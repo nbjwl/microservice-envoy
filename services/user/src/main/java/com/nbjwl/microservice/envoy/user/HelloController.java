4919aaf1d62ac14f87c1378bfa495804f7cbe61d
taki <nbjwl@live.com>
2019-04-03 20:33:41 +0800

    Consul DNS with Envoy

--- services/user/src/main/java/com/nbjwl/microservice/envoy/user/HelloController.java
+++ services/user/src/main/java/com/nbjwl/microservice/envoy/user/HelloController.java
@@ -3,19 +3,18 @@ package com.nbjwl.microservice.envoy.user;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.Inet4Address;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.text.MessageFormat;
+
 @RestController
 public class HelloController
 {
     @GetMapping("/hello")
-    public String hello()
+    public String hello() throws UnknownHostException
     {
-        return "Hello User Service";
-    }
-
-    @GetMapping("/hello/demo")
-    public String helloDemo()
-    {
-        return "Hello Demo";
+        return MessageFormat.format( "Hello User Service {0}", Inet4Address.getLocalHost().getHostAddress());
     }
 
 }
